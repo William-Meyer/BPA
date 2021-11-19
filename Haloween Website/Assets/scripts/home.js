d6dfb9462d6a469e49411c772817b38eadb63cf8
William-Meyer <william.meyer0529@gmail.com>
2021-11-19 10:49:51 -0500

    added halloween folder

---
+++ Haloween Website/Assets/scripts/home.js
@@ -0,0 +1,5 @@
+var myImg = document.querySelector("#homeHeader");
+var currWidth = myImg.clientWidth;
+var currHeight = myImg.clientHeight;
+var con = document.getElementById("#container");
+con.style.width = 900px;
