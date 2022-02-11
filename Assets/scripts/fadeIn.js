b72e06baf34657bfb52d0d177506d41f198b0410
William-Meyer <william.meyer0529@gmail.com>
2022-02-11 11:14:43 -0500

    fade in and out

--- Assets/scripts/fadeIn.js
+++ Assets/scripts/fadeIn.js
@@ -2,10 +2,9 @@
 function isInViewport(el) {
 const rect = el.getBoundingClientRect();
 return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >=3000 || (rect.bottom <= window.innerHeight && rect.bottom >= 0)
+    //rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    //rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
 );
 }
@@ -15,8 +14,12 @@ document.addEventListener('scroll', function() {
   for (let i = 0; i < options.length; i++) {
     var el = options[i];
     var isIn = isInViewport(el);
+    const rect = el.getBoundingClientRect();
     if (isIn == true){
       el.classList.add("fadeInClass");
+    }
+    else if(el.classList.contains("fadeInClass")  &&  (rect.top <= 0)){
+      el.classList.remove("fadeInClass");
     }
   }
 });
