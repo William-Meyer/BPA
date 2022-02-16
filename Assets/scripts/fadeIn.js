467ada7b0fa5ca71464b9da8b19e2d88e6541966
William-Meyer <william.meyer0529@gmail.com>
2022-02-16 10:53:58 -0500

    fade in done

--- Assets/scripts/fadeIn.js
+++ Assets/scripts/fadeIn.js
@@ -1,22 +1,18 @@
 //scrolljs
-function isInViewport(el) {
-const rect = el.getBoundingClientRect();
-return (
-    rect.top >=0 && (rect.bottom <= window.innerHeight && rect.bottom >= 0)
-    //rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    //rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-
-);
+function isOut(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+           (rect.x + rect.width) < 0
+             || (rect.y + rect.height) < 0
+             || (rect.x > window.innerWidth || rect.y > window.innerHeight)
+         );
 }
-
 var options = document.getElementsByClassName('fadeInID');
 document.addEventListener('scroll', function() {
   for (let i = 0; i < options.length; i++) {
     var el = options[i];
-    var isIn = isInViewport(el);
     const rect = el.getBoundingClientRect();
-    if(el.classList.contains("fadeInClass")  && (rect.bottom >= window.innerHeight){
-      alert("test: " + rect.bottom + "   " + window.innerHeight);
+    if(el.classList.contains("fadeInClass")  && (isOut(el))) {
       el.classList.remove("fadeInClass");
     }
     else if (!(el.classList.contains("fadeInClass"))){
