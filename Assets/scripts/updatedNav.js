5c1e62e5a23c2dcfbf035a687994bba23401f8b2
William-Meyer <william.meyer0529@gmail.com>
2022-03-04 16:33:24 -0800

    working on html and js

--- Assets/scripts/updatedNav.js
+++ Assets/scripts/updatedNav.js
@@ -1,11 +1,19 @@
 (function($) {
   $(function() {
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
+    $('nav ul li a:not(:only-child)').hover(function(e) {
+      $(this).siblings('.nav-dropdown').show();
+      $(this).show();
       $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
     });
-    $('html').click(function() {
+    $('.listElement a').hover(function(e){
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      $(this).show();
+    })
+    $('.nav-dropdown').hover(function(e){
+      $(this).show();
+    }
+    $('body').hover(function() {
       $('.nav-dropdown').hide();
     });
     $('#nav-toggle').click(function() {
