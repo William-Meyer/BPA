7dce76297d70e366e7a001d0e053317d7dbb44f5
William-Meyer <william.meyer0529@gmail.com>
2022-03-07 11:14:11 -0500

    nav

--- Assets/scripts/updatedNav.js
+++ Assets/scripts/updatedNav.js
@@ -1,40 +1,101 @@
 (function($) {
   $(function() {
+
+    $('#container').hover(function(){
+      if($(window).width() > 1100){
+      $('#learnContents').hide();
+      $('#RYFContents').hide();
+      $('#resourcesContents').hide();
+    }
+    });
     $('#RYF').hover(function(){
+      if($(window).width() > 1100){
         $('#learnContents').hide();
         $('#RYFContents').show();
         $('#resourcesContents').hide();
+      }
     }
   );
     $('#resources').hover(function(){
+      if($(window).width() > 1100){
         $('#learnContents').hide();
         $('#RYFContents').hide();
-        $('#resourcesContents').show();
+        $('#resourcesContents').show();}
     }
   );
     $('#about').hover(function(){
+      if($(window).width() > 1100){
         $('#learnContents').hide();
         $('#RYFContents').hide();
-        $('#resourcesContents').hide();
+        $('#resourcesContents').hide();}
     }
   );
     $('#quiz').hover(function(){
+      if($(window).width() > 1100){
         $('#learnContents').hide();
         $('#RYFContents').hide();
-        $('#resourcesContents').hide();
+        $('#resourcesContents').hide();}
     }
   );
     $('#learn').hover(function(){
+      if($(window).width() > 1100){
+        $("#learnContents").css("display", "block");
         $('#learnContents').show();
         $('#RYFContents').hide();
-        $('#resourcesContents').hide();
+        $('#resourcesContents').hide();}
     }
   );
-$('body').hover(function(){
-  $('.nav-dropdown').hide();
-});
+  $('#learn').click(function(){
+    if($(window).width() <= 1100){
+      $("#learnContents").css("display", "block");
+      $('#learnContents').show();
+      $('#RYFContents').hide();
+      $('#resourcesContents').hide();
+    }
+  });
+  $('#quiz').click(function(){
+    if($(window).width() <= 1100){
+      $('#learnContents').hide();
+      $('#RYFContents').hide();
+      $('#resourcesContents').hide();
+    }
+  });
+  $('#about').click(function(){
+    if($(window).width() <= 1100){
+      $('#learnContents').hide();
+      $('#RYFContents').hide();
+      $('#resourcesContents').hide();
+    }
+  });
+  $('#resources').click(function(){
+    if($(window).width() <= 1100){
+      $("#resourcesContents").css("display", "block");
+      $('#learnContents').hide();
+      $('#RYFContents').hide();
+      $('#resourcesContents').show();
+    }
+  });
+  $('#RYF').click(function(){
+    if($(window).width() <= 1100){
+      $("#RYFContents").css("display", "block");
+      $('#learnContents').hide();
+      $('#RYFContents').show();
+      $('#resourcesContents').hide();
+    }
+  });
+  $('#container').click(function(){
+    if($(window).width() <= 1100){
+    $('#learnContents').hide();
+    $('#RYFContents').hide();
+    $('#resourcesContents').hide();}
+  });
+
+
     $('#nav-toggle').click(function() {
       $('nav ul').slideToggle();
+      $('#learnContents').hide();
+      $('#RYFContents').hide();
+      $('#resourcesContents').hide();
     });
     $('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
